180c100a2f99884dfc4e663c738e9379e7b91652
seabrgui <guilherme.seabra@diconium.com>
2020-07-24 16:25:20 +0100

    reset home

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -19,7 +19,7 @@ class Header extends Component {
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
          <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">🏠</a></li>
+            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About Me</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
